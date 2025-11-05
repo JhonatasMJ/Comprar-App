3d5e4c93b8ca2d3575e76c15adba3347c95167af
JhonatasMJ <jhonatasjhmj@gmail.com>
2025-11-04 22:06:51 -0300

    colors

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,6 +3,6 @@ import { styles } from "./styles";
 
 export function Input({...rest}: TextInputProps ) { 
     return (
-        <TextInput style={styles.container} {...rest}/>
+        <TextInput placeholderTextColor={"#828282"} style={styles.container} {...rest}/>
     )
 }
